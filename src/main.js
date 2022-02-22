a482bb44185d6d4ff9902d787bd3ee98c4cfb1ff
leishao06 <987313405@qq.com>
2022-02-22 22:11:57 +0800

    配置axios错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
 import App from './App.vue'
 
+// 根据前端的跨域方式做调整(这里用接口代理的方式) 向/a/b发请求，会变成向/api/a/b，之后通过接口代理配置去掉/api => /a/b
+axios.defaults.baseURL = '/api';
+axios.defaults.timeout = 8000; // 超时时间
+
+// 响应拦截器，做接口错误拦截
+axios.interceptors.response.use(function(response) {
+  // 获取接口返回值
+  let res = response.data;
+  // 和后端协调：status为0表示正确
+  if (res.status == 0) {
+    return res.data;
+  } else if(res.status == 10) { // 没登陆时接口报错，status为10
+    // 没有登录的话就跳转到登录页面
+    window.location.href = '/#/login';
+  } else { // 其他错误，弹出错误信息
+    alert(res.msg);
+  }
+})
+
+Vue.prototype.axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
