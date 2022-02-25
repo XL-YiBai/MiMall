36854fc791beb1cc49d50a420f6c12f45c800d15
leishao06 <987313405@qq.com>
2022-02-25 20:31:56 +0800

    封装Modal模态框组件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,11 +7,14 @@
             <li class="menu-item">
               <a href="javascript:;">手机 电话卡</a>
               <div class="children">
-                <ul v-for="(item,i) in menuList" :key="i">
-                  <li v-for="(sub,j) in item" :key="j">
-                    <a :href="sub?'/#/product/'+sub.id:''">
-                      <img :src="sub?sub.img:'/imgs/item-box-1.png'" alt="">
-                      {{sub?sub.name:'小米9'}}
+                <ul v-for="(item, i) in menuList" :key="i">
+                  <li v-for="(sub, j) in item" :key="j">
+                    <a :href="sub ? '/#/product/' + sub.id : ''">
+                      <img
+                        :src="sub ? sub.img : '/imgs/item-box-1.png'"
+                        alt=""
+                      />
+                      {{ sub ? sub.name : "小米9" }}
                     </a>
                   </li>
                 </ul>
@@ -42,7 +45,7 @@
         </div>
         <swiper :options="swiperOption">
           <swiper-slide v-for="(item, index) in slideList" :key="index">
-            <a :href="'/#/product/'+item.id"><img :src="item.img"></a>
+            <a :href="'/#/product/' + item.id"><img :src="item.img" /></a>
           </swiper-slide>
           <div class="swiper-pagination" slot="pagination"></div>
           <div class="swiper-button-prev" slot="button-prev"></div>
@@ -50,13 +53,17 @@
         </swiper>
       </div>
       <div class="ads-box">
-        <a :href="'/#/product/'+item.id" v-for="(item, index) in adsList" :key="index">
-          <img :src="item.img" alt="">
+        <a
+          :href="'/#/product/' + item.id"
+          v-for="(item, index) in adsList"
+          :key="index"
+        >
+          <img :src="item.img" alt="" />
         </a>
       </div>
       <div class="banner">
         <a href="/#/product/30">
-          <img src="/imgs/banner-1.png" alt="">
+          <img src="/imgs/banner-1.png" alt="" />
         </a>
       </div>
     </div>
@@ -66,20 +73,28 @@
         <h2>手机</h2>
         <div class="wrapper">
           <div class="banner-left">
-            <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt=""></a>
+            <a href="/#/product/35"><img src="/imgs/mix-alpha.jpg" alt="" /></a>
           </div>
           <div class="list-box">
             <div class="list" v-for="(arr, i) in phoneList" :key="i">
-              <div class="item" v-for="(item,j) in arr" :key="j">
-                <span v-if="j%2==0" :class="(j%2==0)?'new-pro':'kill-pro'">新品</span>
-                <span v-if="j%2==1" :class="(j%2==0)?'new-pro':'kill-pro'">秒杀</span>
+              <div class="item" v-for="(item, j) in arr" :key="j">
+                <span
+                  v-if="j % 2 == 0"
+                  :class="j % 2 == 0 ? 'new-pro' : 'kill-pro'"
+                  >新品</span
+                >
+                <span
+                  v-if="j % 2 == 1"
+                  :class="j % 2 == 0 ? 'new-pro' : 'kill-pro'"
+                  >秒杀</span
+                >
                 <div class="item-img">
-                  <img :src="item.mainImage" alt="">
+                  <img :src="item.mainImage" alt="" />
                 </div>
                 <div class="item-info">
-                  <h3>{{item.name}}</h3>
-                  <p>{{item.subtitle}}</p>
-                  <p class="price">{{item.price}}元</p>
+                  <h3>{{ item.name }}</h3>
+                  <p>{{ item.subtitle }}</p>
+                  <p class="price">{{ item.price }}元</p>
                 </div>
               </div>
             </div>
@@ -88,292 +103,316 @@
       </div>
     </div>
     <service-bar></service-bar>
+    <Modal 
+      title="提示" 
+      sureText="查看购物车" 
+      btnType="1" 
+      modalType="middle"
+      :showModal="true"
+    >
+      <template v-slot:body>
+        <p>商品添加成功！</p>
+      </template>
+    </Modal>
   </div>
 </template>
 
 <script>
-import ServiceBar from '../components/ServiceBar.vue'
-import { Swiper, SwiperSlide } from 'vue-awesome-swiper'
-import 'swiper/css/swiper.css'
+import ServiceBar from "../components/ServiceBar.vue";
+import Modal from "../components/Modal.vue";
+import { Swiper, SwiperSlide } from "vue-awesome-swiper";
+import "swiper/css/swiper.css";
 export default {
-  name: 'index',
-  components: { Swiper, SwiperSlide, ServiceBar },
+  name: "index",
+  components: { Swiper, SwiperSlide, ServiceBar, Modal },
   data() {
     return {
       swiperOption: {
         autoplay: true,
         loop: true,
-        effect: 'cube',
+        effect: "cube",
         cubeEffect: {
           shadowOffset: 100,
-          shadowScale: 0.6
+          shadowScale: 0.6,
         },
         pagination: {
-          el: '.swiper-pagination',
-          clickable: true
+          el: ".swiper-pagination",
+          clickable: true,
         },
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        }
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
       },
       slideList: [
         {
-          id: '42',
-          img: '/imgs/slider/slide-1.jpg'
+          id: "42",
+          img: "/imgs/slider/slide-1.jpg",
         },
         {
-          id: '45',
-          img: '/imgs/slider/slide-2.jpg'
+          id: "45",
+          img: "/imgs/slider/slide-2.jpg",
         },
         {
-          id: '46',
-          img: '/imgs/slider/slide-3.jpg'
+          id: "46",
+          img: "/imgs/slider/slide-3.jpg",
         },
         {
-          id: '',
-          img: '/imgs/slider/slide-4.jpg'
+          id: "",
+          img: "/imgs/slider/slide-4.jpg",
         },
         {
-          id: '',
-          img: '/imgs/slider/slide-5.jpg'
+          id: "",
+          img: "/imgs/slider/slide-5.jpg",
         },
       ],
       menuList: [
         [
           {
             id: 30,
-            img: '/imgs/item-box-1.png',
-            name: '小米CC9'
-          },{
+            img: "/imgs/item-box-1.png",
+            name: "小米CC9",
+          },
+          {
             id: 31,
-            img: '/imgs/item-box-2.png',
-            name: '小米8青春版'
-          },{
+            img: "/imgs/item-box-2.png",
+            name: "小米8青春版",
+          },
+          {
             id: 32,
-            img: '/imgs/item-box-3.jpg',
-            name: 'Redmi K20 Pro'
-          },{
+            img: "/imgs/item-box-3.jpg",
+            name: "Redmi K20 Pro",
+          },
+          {
             id: 33,
-            img: '/imgs/item-box-4.jpg',
-            name: '移动4G专区'
+            img: "/imgs/item-box-4.jpg",
+            name: "移动4G专区",
           },
         ],
-        [0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
       ],
       adsList: [
         {
           id: 33,
-          img: '/imgs/ads/ads-1.png'
-        }, {
+          img: "/imgs/ads/ads-1.png",
+        },
+        {
           id: 48,
-          img: '/imgs/ads/ads-2.jpg'
-        }, {
+          img: "/imgs/ads/ads-2.jpg",
+        },
+        {
           id: 45,
-          img: '/imgs/ads/ads-3.png'
-        }, {
+          img: "/imgs/ads/ads-3.png",
+        },
+        {
           id: 47,
-          img: '/imgs/ads/ads-4.jpg'
-        }
+          img: "/imgs/ads/ads-4.jpg",
+        },
       ],
-      phoneList: []
-    }
+      phoneList: [],
+    };
   },
   mounted() {
     this.init();
   },
-  methods: {  
+  methods: {
     init() {
-      this.axios.get('/products', {
-        params: {
-          categoryId: 100012,
-          pageSize: 14
-        }
-      }).then((res) => {
-        res.list = res.list.slice(6, 14);
-        // 返回的8条数据生成两行四列的二维数组
-        this.phoneList = [res.list.slice(0,4), res.list.slice(4,8)];
-      })
-    }
-  }
-}
+      this.axios
+        .get("/products", {
+          params: {
+            categoryId: 100012,
+            pageSize: 14,
+          },
+        })
+        .then((res) => {
+          res.list = res.list.slice(6, 14);
+          // 返回的8条数据生成两行四列的二维数组
+          this.phoneList = [res.list.slice(0, 4), res.list.slice(4, 8)];
+        });
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  @import '../assets/scss/config.scss';
-  @import '../assets/scss/mixin.scss';
-  .index {
-    .swiper-box {
-      .nav-menu {
-        position: absolute;
-        width: 264px;
-        height: 451px;
-        z-index: 9;
-        padding: 26px 0;
-        background-color: #55585a7a; 
-        box-sizing: border-box;
-        .menu-wrap {
-          .menu-item {
-            height: 50px;
-            line-height: 50px;
-            a {
-              position: relative;
-              display: block;
-              font-size: 16px;
-              color: #ffffff;
-              padding-left: 30px;
-              &:after {
-                position: absolute;
-                right: 30px;
-                top: 17.5px;
-                content: ' ';
-                @include bgImg(10px, 15px, '/imgs/icon-arrow.png');
-              }
-            }
-            &:hover {
-              background-color: $colorA;
-              .children {
-                display: block;
-              }
-            }
-            .children {
-              display: none;
-              width: 962px;
-              height: 451px;
-              background-color: $colorG;
+@import "../assets/scss/config.scss";
+@import "../assets/scss/mixin.scss";
+.index {
+  .swiper-box {
+    .nav-menu {
+      position: absolute;
+      width: 264px;
+      height: 451px;
+      z-index: 9;
+      padding: 26px 0;
+      background-color: #55585a7a;
+      box-sizing: border-box;
+      .menu-wrap {
+        .menu-item {
+          height: 50px;
+          line-height: 50px;
+          a {
+            position: relative;
+            display: block;
+            font-size: 16px;
+            color: #ffffff;
+            padding-left: 30px;
+            &:after {
               position: absolute;
-              top: 0;
-              left: 264px;
-              border: 1px solid $colorH;
-              ul {
-                display: flex;
-                justify-content: space-between;
+              right: 30px;
+              top: 17.5px;
+              content: " ";
+              @include bgImg(10px, 15px, "/imgs/icon-arrow.png");
+            }
+          }
+          &:hover {
+            background-color: $colorA;
+            .children {
+              display: block;
+            }
+          }
+          .children {
+            display: none;
+            width: 962px;
+            height: 451px;
+            background-color: $colorG;
+            position: absolute;
+            top: 0;
+            left: 264px;
+            border: 1px solid $colorH;
+            ul {
+              display: flex;
+              justify-content: space-between;
+              height: 75px;
+              li {
                 height: 75px;
-                li {
-                  height: 75px;
-                  line-height: 75px;
-                  flex: 1;
-                  padding-left: 23px;
-                }
-                a {
-                  color: $colorB;
-                  font-size: 14px;
-                }
-                img {
-                  width: 42px;
-                  height: 35px;
-                  vertical-align: middle;
-                  margin-right: 15px;
-                }
+                line-height: 75px;
+                flex: 1;
+                padding-left: 23px;
               }
-            }
-          }
-        }
-      }
-      .swiper-container {
-        height: 451px;
-        .swiper-button-prev {
-          left: 274px;
-        }
-        img {
-          width: 100%;
-          height: 100%;
-        }
-      }
-    }
-    .ads-box {
-      @include flex();
-      margin-top: 14px;
-      margin-bottom: 31px;
-      a {
-        width: 296px;
-        height: 167px;
-      }
-    }
-    .banner {
-      margin-bottom: 50px;
-    }
-    .product-box {
-      background-color: $colorJ;
-      padding: 30px 0 50px;
-      h2 {
-        font-size: $fontF;
-        height: 21px;
-        line-height: 21px;
-        color: $colorB;
-        margin-bottom: 20px;
-      }
-      .wrapper {
-        display: flex;
-        .banner-left {
-          margin-right: 16px;
-          img {
-            width: 224px;
-            height: 619px;
-          }
-        }
-        .list-box {
-          .list {
-            @include flex();
-            width: 986px;
-            margin-bottom: 14px;
-            &:last-child {
-              margin-bottom: 0;
-            }
-            .item {
-              width: 236px;
-              height: 302px;
-              background-color: $colorG;
-              text-align: center;
-              span {
-                display: inline-block;
-                width: 67px;
-                height: 24px;
+              a {
+                color: $colorB;
                 font-size: 14px;
-                line-height: 24px;
-                color: $colorG;
-                &.new-pro {
-                  background-color: #7ECF68;
-                }
-                &.kill-pro {
-                  background-color: #E82626;
+              }
+              img {
+                width: 42px;
+                height: 35px;
+                vertical-align: middle;
+                margin-right: 15px;
+              }
+            }
+          }
+        }
+      }
+    }
+    .swiper-container {
+      height: 451px;
+      .swiper-button-prev {
+        left: 274px;
+      }
+      img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+  }
+  .ads-box {
+    @include flex();
+    margin-top: 14px;
+    margin-bottom: 31px;
+    a {
+      width: 296px;
+      height: 167px;
+    }
+  }
+  .banner {
+    margin-bottom: 50px;
+  }
+  .product-box {
+    background-color: $colorJ;
+    padding: 30px 0 50px;
+    h2 {
+      font-size: $fontF;
+      height: 21px;
+      line-height: 21px;
+      color: $colorB;
+      margin-bottom: 20px;
+    }
+    .wrapper {
+      display: flex;
+      .banner-left {
+        margin-right: 16px;
+        img {
+          width: 224px;
+          height: 619px;
+        }
+      }
+      .list-box {
+        .list {
+          @include flex();
+          width: 986px;
+          margin-bottom: 14px;
+          &:last-child {
+            margin-bottom: 0;
+          }
+          .item {
+            width: 236px;
+            height: 302px;
+            background-color: $colorG;
+            text-align: center;
+            span {
+              display: inline-block;
+              width: 67px;
+              height: 24px;
+              font-size: 14px;
+              line-height: 24px;
+              color: $colorG;
+              &.new-pro {
+                background-color: #7ecf68;
+              }
+              &.kill-pro {
+                background-color: #e82626;
+              }
+            }
+            .item-img {
+              img {
+                width: 100%;
+                height: 195px;
+              }
+            }
+            .item-info {
+              h3 {
+                font-size: $fontJ;
+                color: $colorB;
+                line-height: $fontJ;
+                font-weight: bold;
+              }
+              p {
+                color: $colorD;
+                line-height: 13px;
+                margin: 6px auto 13px;
+              }
+              .price {
+                color: #f20a0a;
+                font-size: $fontJ;
+                font-weight: bold;
+                cursor: pointer;
+                &:after {
+                  @include bgImg(22px, 22px, "/imgs/icon-cart-hover.png");
+                  content: " ";
+                  margin-left: 5px;
+                  vertical-align: middle;
                 }
               }
-              .item-img {
-                img {
-                  width: 100%;
-                  height: 195px;
-                }
-              }
-              .item-info {
-                  h3 {
-                    font-size: $fontJ;
-                    color: $colorB;
-                    line-height: $fontJ;
-                    font-weight: bold;
-                  }
-                  p {
-                    color: $colorD;
-                    line-height: 13px;
-                    margin: 6px auto 13px;
-                  }
-                  .price {
-                    color: #F20A0A;
-                    font-size: $fontJ;
-                    font-weight: bold;
-                    cursor: pointer;
-                    &:after {
-                      @include bgImg(22px, 22px, '/imgs/icon-cart-hover.png');
-                      content: ' ';
-                      margin-left: 5px;
-                      vertical-align: middle;
-                    }
-                  }
-                }
             }
           }
         }
       }
     }
   }
+}
 </style>
