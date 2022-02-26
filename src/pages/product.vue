c703d717200e145e26f29d5df4818adaafda8f03
leishao06 <987313405@qq.com>
2022-02-26 17:10:50 +0800

    ProductParam产品参数组件静态实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>
-    product
+  <div class="product">
+    <product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </product-param>
   </div>
 </template>
 
 <script>
+import ProductParam from '../components/ProduectParam.vue'
 export default {
   name: 'product',
+  components: { ProductParam }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .product {
+    button {
+      margin-left: 10px;
+    }
+  }
 </style>
